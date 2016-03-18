c86d40fc5075a3dec0c8321b5e55035de03a19d6
CodeStrider35 <prince1moreno@gmail.com>
2016-03-18 14:59:17 -0700

    Added files via upload

--- heroku_deploy/app.js
+++ heroku_deploy/app.js
@@ -1,11 +1,10 @@
 var http = require('http');
 
-var hostname = '127.0.0.1';
 var port = process.env.PORT || 1337;
 
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(port, hostname, function() {
-   console.log("Sever running at http://" + hostname + ":" + port + "/");
+}).listen(port, function() {
+   console.log("Sever running at http://:" + port + "/");
 });
